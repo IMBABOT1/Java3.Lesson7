acc5459e929b3e3da1dc713551a3dd1cf9259fda
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-24 19:28:08 +0300

    HashMap

--- src/main/java/TestStarter.java
+++ src/main/java/TestStarter.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class TestStarter {
 
     private static void start(Class test) throws Exception {
-        HashMap<Class, Integer> map = new HashMap<>();
+        HashMap<Integer, Class> map = new HashMap<>();
         int beforeCount = 0;
         int afterCount = 0;
         int p = 0;
@@ -14,34 +14,29 @@ public class TestStarter {
         Class c = TestClass.class;
         Method[] methods = c.getDeclaredMethods();
         for (Method m : methods) {
-            if (m.isAnnotationPresent(BeforeSuite.class)){
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
                 beforeCount++;
             }
-            if (m.isAnnotationPresent(AfterSuite.class)){
+            if (m.isAnnotationPresent(AfterSuite.class)) {
                 afterCount++;
             }
-            if (beforeCount > 1 || afterCount > 1){
+            if (beforeCount > 1 || afterCount > 1) {
                 throw new RuntimeException("Beforecount: " + beforeCount + " " + "Aftercount" + afterCount);
             }
-            if (m.isAnnotationPresent(BeforeSuite.class)){
-                map.put(BeforeSuite.class, m.getAnnotation(BeforeSuite.class).priority());
+            if (m.isAnnotationPresent(BeforeSuite.class)) {
+                map.put(m.getAnnotation(BeforeSuite.class).priority(), BeforeSuite.class);
             }
-            if (m.isAnnotationPresent(AfterSuite.class)){
-                map.put(AfterSuite.class, m.getAnnotation(AfterSuite.class).priority());
+            if (m.isAnnotationPresent(AfterSuite.class)) {
+                map.put(m.getAnnotation(AfterSuite.class).priority(), AfterSuite.class);
             }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 9){
-                map.put(Test.class, m.getAnnotation(Test.class).priority());
+            if (m.isAnnotationPresent(Test.class)) {
+                map.put(m.getAnnotation(Test.class).priority(), Test.class);
             }
-
-
         }
+
         System.out.println(map);
+
     }
-
-
-
-
-
 
 
     public static void main(String[] args) {
