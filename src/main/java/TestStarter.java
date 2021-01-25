d9b74261b56f0436ab7edc0b36c4dc0e23127173
Mikhail Shemyakin <zlovred12@gmail.com>
2021-01-25 10:10:18 +0300

    sort

--- src/main/java/TestStarter.java
+++ src/main/java/TestStarter.java
@@ -13,8 +13,6 @@ public class TestStarter {
         Class c = TestClass.class;
         Method[] methods = c.getDeclaredMethods();
 
-
-        Method[] temp = new Method[methods.length];
         for (Method m : methods) {
             if (m.isAnnotationPresent(BeforeSuite.class)) {
                 beforeCount++;
@@ -22,46 +20,21 @@ public class TestStarter {
             if (m.isAnnotationPresent(AfterSuite.class)) {
                 afterCount++;
             }
-            if (beforeCount > 1 || afterCount > 1) {
-                throw new RuntimeException("Beforecount: " + beforeCount + " " + "Aftercount" + afterCount);
-            }
-            if (m.isAnnotationPresent(BeforeSuite.class) && m.getAnnotation(BeforeSuite.class).priority() == 10) {
-                temp[9] = m;
-            }
-            if (m.isAnnotationPresent(AfterSuite.class) && m.getAnnotation(AfterSuite.class).priority() == 1) {
-                temp[0] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 2) {
-                temp[1] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 3) {
-                temp[2] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 4) {
-                temp[3] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 5) {
-                temp[4] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 6) {
-                temp[5] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 7) {
-                temp[6] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 8) {
-                temp[7] = m;
-            }
-            if (m.isAnnotationPresent(Test.class) && m.getAnnotation(Test.class).priority() == 9) {
-                temp[8] = m;
-            }
         }
 
-
-        for (int i = temp.length - 1; i >= 0; i--) {
-            temp[i].invoke(null);
+        for (int i = 0; i < methods.length - 1 ; i++) {
+            for (int j = 0; j < methods.length - 1 - i; j++) {
+               if (methods[j].getAnnotation(Test.class).priority() > methods[j+1].getAnnotation(Test.class).priority()){
+                   Method temp = methods[j];
+                   methods[j] = methods[j + 1];
+                   methods[j+1] = temp;
+               }
+            }
         }
     }
+
+
+
 
 
     public static void main(String[] args) {
